f604eae24cdf7e9f7c69c2168dbe05a68564fe1c
Sneckan <paccelito@gmail.com>
2018-11-19 14:02:25 +0100

    Add project files.

---
+++ WowGuildApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WowGuildApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
