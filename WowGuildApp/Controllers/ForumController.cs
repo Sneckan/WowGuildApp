90a7d539e3aec1c735923ddaf12219fb16619a91
aldinhro <33513597+aldinhro@users.noreply.github.com>
2018-11-25 16:19:14 +0100

    fixed forum controller

--- WowGuildApp/Controllers/ForumController.cs
+++ WowGuildApp/Controllers/ForumController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -12,17 +13,40 @@ namespace WowGuildApp
 {
     public class ForumController : Controller
     {
-        private readonly ApplicationDbContext _context;
+        private readonly ApplicationDbContext db;
 
         public ForumController(ApplicationDbContext context)
         {
-            _context = context;
+            db = context;
+        }
+
+        public enum Categories
+        {
+            [Description("News")]
+            News = 1,
+            [Description("Raid Discussion")]
+            RaidDiscussion,
+            [Description("General")]
+            General,
+            [Description("Classes")]
+            Classes,
+            [Description("Off topic")]
+            OffTopic,
+            [Description("Recruitment")]
+            Recruitment
+        }
+
+        [Route("Forum/{category}")]
+        public IActionResult Category(string category)
+        {
+            var list = db.Posts.Where(p => p.Category == category);
+            return View(list);
         }
 
         // GET: Forum
         public async Task<IActionResult> Index()
         {
-            return View(await _context.Posts.ToListAsync());
+            return View(await db.Posts.ToListAsync());
         }
 
         // GET: Forum/Details/5
@@ -33,7 +57,7 @@ namespace WowGuildApp
                 return NotFound();
             }
 
-            var post = await _context.Posts
+            var post = await db.Posts
                 .FirstOrDefaultAsync(m => m.Id == id);
             if (post == null)
             {
@@ -54,12 +78,12 @@ namespace WowGuildApp
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Title,Content,Date")] Post post)
+        public async Task<IActionResult> Create([Bind("Id,Title,Category,Content,Date")] Post post)
         {
             if (ModelState.IsValid)
             {
-                _context.Add(post);
-                await _context.SaveChangesAsync();
+                db.Add(post);
+                await db.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
             return View(post);
@@ -73,7 +97,7 @@ namespace WowGuildApp
                 return NotFound();
             }
 
-            var post = await _context.Posts.FindAsync(id);
+            var post = await db.Posts.FindAsync(id);
             if (post == null)
             {
                 return NotFound();
@@ -97,8 +121,8 @@ namespace WowGuildApp
             {
                 try
                 {
-                    _context.Update(post);
-                    await _context.SaveChangesAsync();
+                    db.Update(post);
+                    await db.SaveChangesAsync();
                 }
                 catch (DbUpdateConcurrencyException)
                 {
@@ -124,7 +148,7 @@ namespace WowGuildApp
                 return NotFound();
             }
 
-            var post = await _context.Posts
+            var post = await db.Posts
                 .FirstOrDefaultAsync(m => m.Id == id);
             if (post == null)
             {
@@ -139,15 +163,15 @@ namespace WowGuildApp
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            var post = await _context.Posts.FindAsync(id);
-            _context.Posts.Remove(post);
-            await _context.SaveChangesAsync();
+            var post = await db.Posts.FindAsync(id);
+            db.Posts.Remove(post);
+            await db.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
 
         private bool PostExists(int id)
         {
-            return _context.Posts.Any(e => e.Id == id);
+            return db.Posts.Any(e => e.Id == id);
         }
     }
 }
